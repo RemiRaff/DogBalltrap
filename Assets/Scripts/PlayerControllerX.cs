f1553a9a77c72d9f4294de59fedbae3523b4cf7b
RemiRaff <remi_raffaghello@hotmail.com>
2023-01-10 15:35:01 +0100

    Debug message added for ball at floor

--- Assets/Scripts/PlayerControllerX.cs
+++ Assets/Scripts/PlayerControllerX.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; // pour DateTime
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+
+    private DateTime lastSpawnTime;
+    private DateTime maxSpawnDelay;
+
+    private void Start()
+    {
+        lastSpawnTime = DateTime.Now;
+    }
 
     // Update is called once per frame
     void Update()
@@ -12,7 +21,13 @@ public class PlayerControllerX : MonoBehaviour
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            TimeSpan time = lastSpawnTime - DateTime.Now;
+            Debug.Log(time.ToString());
+
+            // si c pluq que le delay on spawn et met à jour le temps
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawnTime = DateTime.Now;
+
         }
     }
 }
